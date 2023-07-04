4e42bf84f434a2e287f528e8df67623da7c5a846
engatef2012 <mazenmizo783@gmail.com>
2023-07-04 23:58:31 +0300

    add graphql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { AppRoutingModule}from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CitiesComponent } from './cities/cities.component'
-import { AngularMaterialModule} from './angular-material.module';
+import { AngularMaterialModule } from './angular-material.module';
 import { CountriesComponent } from './countries/countries.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CityEditComponent } from './cities/city-edit.component';
 import { CountryEditComponent } from './countries/country-edit.component';
 import { LoginComponent } from './auth/login.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor} from './auth/auth.interceptor'
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -38,7 +42,25 @@ import { AuthInterceptor} from './auth/auth.interceptor'
   providers: [{
     provide: HTTP_INTERCEPTORS,
     useClass: AuthInterceptor,
-    multi:true
+    multi: true
+  },
+  {
+    provide: APOLLO_OPTIONS,
+    useFactory: (http: HttpLink) => {
+      return {
+        cache: new InMemoryCache({
+          addTypename: false
+        }),
+        link: http.create({
+          uri: environment.baseUrl + 'api/graphql'
+        }),
+        defaultOptions: {
+          watchQuery: { fetchPolicy: 'no-cache' },
+          query: { fetchPolicy: 'no-cache' }
+        }
+      };
+    },
+    deps: [HttpLink]
   }],
   bootstrap: [AppComponent]
 })
--- src/app/cities/city.service.ts
+++ src/app/cities/city.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { ApiResult, BaseService } from '../base.service';
 import { Country } from '../countries/country';
-import {City } from './city'
+import { City } from './city';
+import { Apollo, gql } from 'apollo-angular'
 @Injectable({
   providedIn: 'root'
 })
 export class CityService extends BaseService<City> {
-  constructor(http:HttpClient) {
-      super(http);
+  constructor(http: HttpClient, private apollo: Apollo) {
+    super(http);
   }
   getData(pageIndex: number, pageSize: number, sortColumn: string, sortOrder: string, filterColumn: string | null, filterQuery: string | null): Observable<ApiResult<City>> {
     var url = this.getUrl('api/cities');
@@ -20,16 +21,35 @@ export class CityService extends BaseService<City> {
       .set("sortOrder", sortOrder)
     if (filterColumn && filterQuery) {
       params = params.set("filterColumn", filterColumn)
-      .set("filterQuery",filterQuery)
+        .set("filterQuery", filterQuery)
     }
-    return this.http.get<ApiResult<City>>(url, { params }); 
+    return this.http.get<ApiResult<City>>(url, { params });
   }
+
   get(id: number): Observable<City> {
-    var url = this.getUrl('api/cities/' + id);
-    return this.http.get<City>(url);
+    //var url = this.getUrl('api/cities/' + id);
+    //return this.http.get<City>(url);
+    return this.apollo.query({
+      query: gql`
+      query GetCityById($id: Int!) {
+        cities(where: { id: { eq: $id } }) {
+          nodes {
+            id
+            name
+            lat
+            lon
+            countryId
+          }
+          }
+          } `,
+      variables: {
+        id
+      }
+    }).pipe(map((result: any) => result.data.cities.nodes[0]));
   }
+
   put(item: City): Observable<City> {
-    var url = this.getUrl('api/cities/'+item.id);
+    var url = this.getUrl('api/cities/' + item.id);
     return this.http.put<City>(url, item);
   }
   post(item: City): Observable<City> {
@@ -41,7 +61,7 @@ export class CityService extends BaseService<City> {
     sortColumn: string,
     sortOrder: string,
     filterColumn: string | null,
-    filterQuery: string | null): Observable<ApiResult<Country>>{
+    filterQuery: string | null): Observable<ApiResult<Country>> {
     var url = this.getUrl("api/Countries");
     var params = new HttpParams()
       .set("pageIndex", pageIndex.toString())
@@ -59,5 +79,5 @@ export class CityService extends BaseService<City> {
     var url = this.getUrl("api/Cities/isDupeCity");
     return this.http.post<boolean>(url, item);
   }
- }
+}
 
